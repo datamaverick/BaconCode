30dcbe97fc5a4d4792e897005ec38c1e5291644b
grantdoan <grant.n.doan@gmail.com>
2015-06-07 22:52:47 -0400

    msg for KeyError

--- decrypt.py
+++ decrypt.py
@@ -2,6 +2,7 @@ import index
 
 #input the message to decode and evaluate its length
 user_input = raw_input("Enter the code here: ")
+
 temp_list = index.remove_space(user_input)
 while len(temp_list) % 5 != 0:
 	print("The total characters must be multiples of 5 for Bacon codes")
@@ -12,10 +13,18 @@ index.assign_number(temp_list)
 
 num_list = index.binary_rep(temp_list)
 
-inter_list = [index.numbers[x] for x in num_list]
-#print(inter_list)
+try: 
+	""" To handle mistyping of upper/lowercase letters, which won't find a key
+	in the dictionary
+	"""
 
-decode_list = [index.alphabets[x] for x in inter_list]
+	inter_list = [index.numbers[x] for x in num_list]
 
-print "The hidden message is:"
-print(decode_list)
+	decode_list = [index.alphabets[x] for x in inter_list]
+
+	print "The hidden message is:"
+	print(decode_list)
+
+except KeyError:
+	print("You mistyped an upper or lower case letter")
+	exit
