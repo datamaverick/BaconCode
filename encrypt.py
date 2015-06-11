a6bcbc4105a77828c27443be48f711b5bae43823
Grant Doan <grant.n.doan@gmail.com>
2015-06-11 12:43:42 -0400

    positions of indices
    explanation for the position list

--- encrypt.py
+++ encrypt.py
@@ -12,7 +12,6 @@ if not all(i.isalpha() for i in inter_list):
 #assigns a number to each letter
 num_list = [index.inv_alphabets[x] for x in inter_list]
 
-
 #return the binary representation of each number in the list
 binary_list = [index.inv_numbers[x] for x in num_list]
 
@@ -30,6 +29,7 @@ while len(temp_list) != letter_count:
 	msg_input = raw_input("Please re-enter a valid message\n > ")
 	temp_list = index.remove_space(msg_input)
 
+#position is a list that contains the indices of letters that need to be capitalized in the message
 position = index.position_list(format_list)
 
 encrypt_str = "".join(c.upper() if i in position else c for i, c in enumerate(temp_list))
